cb9ad784440e691583331d67518b3416749afc3f
David Fuentes <fuentesdt@gmail.com>
2013-05-30 23:07:21 -0500

    running... need to look at result

--- driver.m
+++ driver.m
@@ -1,15 +1,15 @@
-
 clear all
 close all
-
-
 load test.mat
 
-options = optimset('display','off');
+options = optimset('display','iter','Jacobian','on');
 ydata = double(image(50:100,50:100,:));
-xdata = 40.*[1:12]';
+xdata = 40.*[1:size(ydata,3)]';
 % allocate space
-%x0 = zeros(size(x,1),size(x,2),3);
-x0 = rand(size(ydata,1),size(ydata,2),3);
-%[x,resnorm,residual,exitflag, output]=lsqcurvefit(@t2decay,x0,xdata,ydata,LB,UB,options);
-[x,resnorm,residual,exitflag, output]=lsqcurvefit(@t2decay,x0,xdata,ydata);
+%   array shape wrt jacobian of matrix function and variables, see:
+% http://www.mathworks.com/help/optim/ug/writing-objective-functions.html#brkjub4
+x0 = rand(3,size(ydata,1),size(ydata,2));
+UB = inf(3,size(ydata,1),size(ydata,2));
+LB = -inf(3,size(ydata,1),size(ydata,2));
+[x,resnorm,residual,exitflag, output]=lsqcurvefit(@t2decay,x0,xdata,ydata,LB,UB,options);
+%[x,resnorm,residual,exitflag, output]=lsqcurvefit(@t2decay,x0,xdata,ydata);
