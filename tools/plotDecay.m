931e8e63b7cdbd5dc025101743582071e1795088
Florian Maier <fmaier@mdanderson.org>
2013-06-18 10:50:42 -0500

    Updated lines.

--- tools/plotDecay.m
+++ tools/plotDecay.m
@@ -1,14 +1,18 @@
 %% Plot T2 decay.
 % Copyright (c) The University of Texas MD Anderson Cancer Center, 2013
 % Authors: David Fuentes, Florian Maier
-function plotDecay(x,y,xdata,ydata,solutionPixel,solutionVector,solutionVectorChunks)
+function plotDecay( x, y, xdata, ydata, solutionPixel, solutionVector, solutionVectorChunks )
 
     % Plot results.
     pixel = [x,y];
     figure;
-    plot(xdata,squeeze(ydata(pixel(1),pixel(2),:)),'+',...
-         1:max(xdata),solutionPixel(1,pixel(1),pixel(2))* exp(-[1:max(xdata)]/solutionPixel(2,pixel(1),pixel(2))),'g-',...
-         1:max(xdata),solutionVector(1,pixel(1),pixel(2))* exp(-[1:max(xdata)]/solutionVector(2,pixel(1),pixel(2))),'b-',...
-         1:max(xdata),solutionVectorChunks(1,pixel(1),pixel(2))* exp(-[1:max(xdata)]/solutionVectorChunks(2,pixel(1),pixel(2))),'r-')
+    plot(xdata,squeeze(ydata(pixel(1),pixel(2),:)),'k+',...
+         1:max(xdata),solutionPixel(1,pixel(1),pixel(2))* exp(-[1:max(xdata)]/solutionPixel(2,pixel(1),pixel(2))),'r-',...
+         1:max(xdata),solutionVector(1,pixel(1),pixel(2))* exp(-[1:max(xdata)]/solutionVector(2,pixel(1),pixel(2))),'g-',...
+         1:max(xdata),solutionVectorChunks(1,pixel(1),pixel(2))* exp(-[1:max(xdata)]/solutionVectorChunks(2,pixel(1),pixel(2))),'b--')
+    title(sprintf('T_2 Decay at Pixel (%d/%d)', x, y));
+    xlabel('echo time / ms');
+    ylabel('signal / a.u.');
+    legend('measurement', 'pixel-by-pixel fit', 'vector fit', 'block fit');
 
 end
