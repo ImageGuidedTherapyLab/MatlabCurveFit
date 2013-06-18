84b357c016638198f1907fef4b0730abb484a539
Florian Maier <fmaier@mdanderson.org>
2013-06-18 11:35:45 -0500

    Added RMSE calculation.

--- tools/plotDecay.m
+++ tools/plotDecay.m
@@ -3,15 +3,15 @@
 % Authors: David Fuentes, Florian Maier
 function plotDecay( x, y, xdata, ydata, solutionPixel, solutionVector, solutionVectorChunks )
 
-    % Calculate RMSE for this pixel.
-    pixel    = [ x, y ];
+    % Init.
+    pixel    = [ y, x ];
     measured = squeeze( ydata( pixel(1), pixel(2), : ));
     t        = 0:1.1*max(xdata);
     func     = @(t,param) param(1) * exp( - t ./ param(2));
     
     % Plot results.
     figure;
-    plot(xdata,measured,'k+',...
+    plot(xdata ,measured, 'k+',...
          t, func( t, solutionPixel(:,pixel(1),pixel(2)) ), 'r-', ...
          t, func( t, solutionVector(:,pixel(1),pixel(2)) ),'g-',...
          t, func( t, solutionVectorChunks(:,pixel(1),pixel(2)) ),'b--')
@@ -19,5 +19,16 @@ function plotDecay( x, y, xdata, ydata, solutionPixel, solutionVector, solutionV
     xlabel('echo time / ms');
     ylabel('signal / a.u.');
     legend('measurement', 'pixel-by-pixel fit', 'vector fit', 'block fit');
+    snapnow;
+    
+    % RMSE calculations.
+    rmsePixel = sqrt(sum(( measured - func( xdata, solutionPixel(:,pixel(1),pixel(2)) ) ).^2)/numel(xdata));
+    rmseVector = sqrt(sum(( measured - func( xdata, solutionVector(:,pixel(1),pixel(2)) ) ).^2)/numel(xdata));
+    rmseVectorChunks = sqrt(sum(( measured - func( xdata, solutionVectorChunks(:,pixel(1),pixel(2)) ) ).^2)/numel(xdata));
+    
+    fprintf('\nRMSEs of pixel (%d/%d)\n', x, y);
+    fprintf('  pixel-by-pixel, parfor:                    % 7.3f\n', rmsePixel);
+    fprintf('  vector, simultaneous fit:                  % 7.3f\n', rmseVector);
+    fprintf('  vector chunks, piecewise simultaneous fit: % 7.3f\n', rmseVectorChunks);
 
 end
