afd4abd9a86a6f5ccfac495bde809af2a1e686b1
Florian Maier <fmaier@mdanderson.org>
2013-06-18 16:47:05 -0500

    Updated visualization of T1 recovery plots.

--- tools/plotRecovery.m
+++ tools/plotRecovery.m
@@ -4,12 +4,51 @@
 
 function plotRecovery(x,y,xdata,ydata,solutionPixel,solutionVector,solutionVectorChunks)
 
+    % Init.
+    pixel    = [ y, x ];
+    measured = squeeze( ydata( pixel(1), pixel(2), : ));
+    t        = 0:1.1*max(xdata);
+    func     = @(t,param) param(1) * abs(1 - 2 * exp(-t ./ param(2)));
+    
     % Plot results.
-    pixel = [y,x];
     figure;
-    plot(xdata,squeeze(ydata(pixel(1),pixel(2),:)),'+',...
-         1:max(xdata),solutionPixel(1,pixel(1),pixel(2))* abs(1 - 2 * exp(-[1:max(xdata)]/solutionPixel(2,pixel(1),pixel(2)))),'g-',...
-         1:max(xdata),solutionVector(1,pixel(1),pixel(2))* abs(1 - 2 * exp(-[1:max(xdata)]/solutionVector(2,pixel(1),pixel(2)))),'b-',...
-         1:max(xdata),solutionVectorChunks(1,pixel(1),pixel(2))* abs(1 - 2 * exp(-[1:max(xdata)]/solutionVectorChunks(2,pixel(1),pixel(2)))),'r-');
+    imagesc(ydata(:,:,1));
+    title('magnitude image');
+    colormap gray;
+    xlabel('x / px');
+    ylabel('y / px');
+    caxis([0 800]);
+    h = colorbar;
+    set(get(h,'ylabel'),'String', 'signal magnitude / a.u.');
+    hold on;
+    plot( x, y, 'y+', 'MarkerSize', 10);
+    snapnow;
+    
+    figure;
+    plot(xdata ,measured, 'k+',...
+         t, func( t, solutionPixel(:,pixel(1),pixel(2)) ), 'r-', ...
+         t, func( t, solutionVector(:,pixel(1),pixel(2)) ),'g-', ...
+         t, func( t, solutionVectorChunks(:,pixel(1),pixel(2)) ),'b--')
+    title(sprintf('T_1 decay at pixel (%d/%d)', x, y));
+    xlabel('echo time / ms');
+    ylabel('signal / a.u.');
+    legend('measurement', 'pixel-by-pixel fit', 'simultaneous fit', 'piecewise simultaneous fit');
+    snapnow;
+    
+    % RMSE calculations.
+    rmsePixel        = rmse( measured, func( xdata, solutionPixel(:,pixel(1),pixel(2))));
+    rmseVector       = rmse( measured, func( xdata, solutionVector(:,pixel(1),pixel(2))));
+    rmseVectorChunks = rmse( measured, func( xdata, solutionVectorChunks(:,pixel(1),pixel(2))));
+    
+    fprintf('\n');
+    fprintf('Estimated T1 of pixel (%d/%d)\n', x, y);
+    fprintf('  pixel-by-pixel, parfor:                    % 4.1f ms\n', solutionPixel(2, pixel(1), pixel(2)));
+    fprintf('  vector, simultaneous fit:                  % 4.1f ms\n', solutionVector(2, pixel(1), pixel(2)));
+    fprintf('  vector chunks, piecewise simultaneous fit: % 4.1f ms\n', solutionVectorChunks(2, pixel(1), pixel(2)));
+    fprintf('\n');
+    fprintf('RMSEs of pixel (%d/%d)\n', x, y);
+    fprintf('  pixel-by-pixel, parfor:                    % 7.3f\n', rmsePixel);
+    fprintf('  vector, simultaneous fit:                  % 7.3f\n', rmseVector);
+    fprintf('  vector chunks, piecewise simultaneous fit: % 7.3f\n', rmseVectorChunks);
 
 end
